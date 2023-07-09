e814ab6d6fa0d3adc439de9c832b2ee6444f62c0
Raffaele Rossi <rossi.raffaele@gmail.com>
2023-07-09 18:35:58 +0100

    emit object files

--- dep0/app/dep0/main.cpp
+++ dep0/app/dep0/main.cpp
@@ -7,9 +7,16 @@
 #include <llvm/IR/IRPrintingPasses.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Support/CommandLine.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/Host.h>
 #include <llvm/Support/InitLLVM.h>
+#include <llvm/Support/TargetRegistry.h>
+#include <llvm/Support/TargetSelect.h>
 #include <llvm/Support/ToolOutputFile.h>
 #include <llvm/Support/WithColor.h>
+#include <llvm/Target/TargetMachine.h>
+
+static llvm::codegen::RegisterCodeGenFlags WTF;
 
 int main(int argc, char** argv)
 {
@@ -28,9 +35,46 @@ int main(int argc, char** argv)
             llvm::cl::desc("Override target triple for module"));
 
     llvm::InitLLVM LLVM(argc, argv);
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllAsmPrinters();
+    llvm::InitializeAllAsmParsers();
     llvm::cl::ParseCommandLineOptions(argc, argv, "dep0 - DepC Bootstrapping Compiler");
 
+    std::string err;
+    auto target_triple =
+        llvm::Triple(
+            mtriple.empty()
+            ? llvm::sys::getDefaultTargetTriple()
+            : llvm::Triple::normalize(mtriple));
+    llvm::Target const* const target = llvm::TargetRegistry::lookupTarget(llvm::codegen::getMArch(), target_triple, err);
+    if (not target)
+    {
+        llvm::WithColor::error(llvm::errs()) << err << '\n';
+        return 1;
+    }
+    llvm::TargetMachine* const machine =
+        target->createTargetMachine(
+            target_triple.getTriple(),
+            llvm::codegen::getCPUStr(),
+            llvm::codegen::getFeaturesStr(),
+            llvm::codegen::InitTargetOptionsFromCodeGenFlags(target_triple),
+            llvm::Optional<llvm::Reloc::Model>(llvm::codegen::getRelocModel()));
+    if (not machine)
+    {
+        llvm::WithColor::error(llvm::errs()) << "Failed to create target machine\n";
+        return 1;
+    }
     llvm::LLVMContext llvm_context;
+    auto const file_type = llvm::codegen::getFileType();
+    auto const oflags =
+        file_type == llvm::CodeGenFileType::CGFT_AssemblyFile
+        ? llvm::sys::fs::OF_Text
+        : llvm::sys::fs::OF_None;
+    auto const suffix =
+        file_type == llvm::CodeGenFileType::CGFT_AssemblyFile
+        ? (emit_llvm ? ".ll" : ".s")
+        : ".o";
     for (auto const& f: input_files)
     {
         auto parse_tree = dep0::parser::parse(f);
@@ -57,7 +101,20 @@ int main(int argc, char** argv)
             llvm::WithColor::error(llvm::errs(), f) << str.str() << '\n';
             return 1;
         }
-        llvm::WithColor::note(llvm::outs(), f) << "Should now emit the object file for " << f << '\n';
+        if (file_type == llvm::CodeGenFileType::CGFT_Null)
+            continue;
+        std::error_code ec;
+        auto out = llvm::ToolOutputFile(f + suffix, ec, oflags);
+        llvm::legacy::PassManager pass_manager;
+        if (file_type == llvm::CodeGenFileType::CGFT_AssemblyFile and emit_llvm)
+            pass_manager.add(llvm::createPrintModulePass(out.os()));
+        else if (machine->addPassesToEmitFile(pass_manager, out.os(), nullptr, file_type))
+        {
+            llvm::WithColor::error(llvm::errs(), f) << "No support for file type\n";
+            return 1;
+        }
+        pass_manager.run(llvm_module->get());
+        out.keep();
     }
     return 0;
 }

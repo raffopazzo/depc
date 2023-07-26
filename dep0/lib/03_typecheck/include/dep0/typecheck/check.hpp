82fa28b785816860edc2be23c264d853be9aab18
Raffaele Rossi <rossi.raffaele@gmail.com>
2023-07-26 20:32:46 +0100

    remove link to TT library

--- dep0/lib/03_typecheck/include/dep0/typecheck/check.hpp
+++ dep0/lib/03_typecheck/include/dep0/typecheck/check.hpp
@@ -1,14 +1,9 @@
 #pragma once
 
-#include "dep0/typecheck/tt/context.hpp"
 #include "dep0/typecheck/ast.hpp"
 #include "dep0/typecheck/error.hpp"
 
 #include "dep0/parser/ast.hpp"
-
-#include "dep0/error.hpp"
-
-#include <optional>
 
 namespace dep0::typecheck {
 

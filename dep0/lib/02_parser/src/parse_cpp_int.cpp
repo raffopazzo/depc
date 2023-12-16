8588f46ecfb74f45ff1c40a355ebdfeed8f6fc6b
Raffaele Rossi <rossi.raffaele@gmail.com>
2023-12-16 18:58:23 +0000

    allocation free parsing of cpp_int (#18)

---
+++ dep0/lib/02_parser/src/parse_cpp_int.cpp
@@ -0,0 +1,30 @@
+#include "private/parse_cpp_int.hpp"
+
+#include "dep0/digit_separator.hpp"
+
+#include <boost/iostreams/device/array.hpp>
+#include <boost/iostreams/stream.hpp>
+
+#include <sstream>
+
+namespace dep0::parser {
+
+boost::multiprecision::cpp_int parse_cpp_int(std::string_view txt)
+{
+    boost::multiprecision::cpp_int value;
+    while (txt.starts_with('0'))
+        txt.remove_prefix(1);
+    if (dep0::contains_digit_separator(txt))
+    {
+        auto s = std::istringstream(dep0::remove_digit_separator(txt));
+        s >> value;
+    }
+    else
+    {
+        auto s = boost::iostreams::stream<boost::iostreams::basic_array_source<char>>(txt.begin(), txt.size());
+        s >> value;
+    }
+    return value;
+}
+
+} // namespace dep0::parser

7f701bcf8cfbd55eb15464a7f47f88b425520180
Raffaele Rossi <rossi.raffaele@gmail.com>
2024-01-27 20:19:48 +0000

    docs

--- dep0/lib/05_llvmgen/include/dep0/llvmgen/gen.hpp
+++ dep0/lib/05_llvmgen/include/dep0/llvmgen/gen.hpp
@@ -13,13 +13,30 @@
 
 namespace dep0::llvmgen {
 
+/**
+ * This enum is passed to @see gen to control whether verification of the LLVM module should be performed or skipped.
+ * Skipping verification is useful when debugging the codegen logic,
+ * allowing the broken module to be saved to a file for manual inspection.
+ */
 enum class verify_t { yes, no };
 
+/**
+ * Generate an LLVM module from a legal module; optionally skip verification of the LLVM module.
+ *
+ * @param ctx           The LLVM context used during codegen; it holds LLVM types, the target machine, etc.
+ * @param module_name   The name to assign to the generated LLVM module.
+ * @param module        The legal DepC module for which to emit an LLVM module.
+ * @param verify        Controls whether verification of the LLVM module should be performed (default) or skipped;
+ *                      skipping verification is useful when debugging the codegen logic,
+ *                      allowing the broken module to be saved to a file for manual inspection.
+ *
+ * @return The generated LLVM module or an error; if verification was performed this will be a valid LLVM module.
+ */
 expected<unique_ref<llvm::Module>> gen(
-    llvm::LLVMContext&,
+    llvm::LLVMContext& ctx,
     std::string_view module_name,
-    typecheck::module_t const& m,
-    verify_t = verify_t::yes
+    typecheck::module_t const& module,
+    verify_t verify = verify_t::yes
 ) noexcept;
 
 }

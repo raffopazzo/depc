f4e9f30a3afa708a18651fdf15548503f429232a
Raffaele Rossi <rossi.raffaele@gmail.com>
2024-01-28 16:14:00 +0000

    add docs

--- dep0/lib/01_ast/include/dep0/ast/beta_delta_reduction.hpp
+++ dep0/lib/01_ast/include/dep0/ast/beta_delta_reduction.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file beta_delta_reduction.hpp
+ * @brief A family of functions that perform a combined beta-delta normalization.
+ *
+ * The normalization strategy is to first apply a full beta-normalization,
+ * which should help in reducing the size of the AST.
+ * If no more beta-normalizations are possible, try to perform a single one-step delta-reduction,
+ * (arguably it should be called a delta-expansion) which might unblock further beta-normalizations.
+ * If delta-reduction was unsuccessful, then we have reached a beta-delta normal-form.
+ */
+
 #pragma once
 
 #include "dep0/ast/ast.hpp"

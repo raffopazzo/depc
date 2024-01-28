79d701567c9fbbdac16ba6b97bb6ccc39fb25d3d
Raffaele Rossi <rossi.raffaele@gmail.com>
2024-01-28 01:37:42 +0000

    doxygen

--- dep0/lib/05_llvmgen/src/private/gen_val.hpp
+++ dep0/lib/05_llvmgen/src/private/gen_val.hpp
@@ -9,12 +9,31 @@
 
 namespace dep0::llvmgen {
 
+/**
+ * Generates an LLVM value for the given expression.
+ *
+ * @param dest  If this parameter is not nullptr, this function will emit appropriate IR instructions
+ *              to store/memcpy/memset the generated value at the runtime location pointed by this LLVM value,
+ *              which must therefore be of an appropriate pointer type.
+ *
+ * @return The value generated or `dest` if it was not nullptr.
+ */
 llvm::Value* gen_val(
     global_context_t&,
     local_context_t const&,
     llvm::IRBuilder<>&,
     typecheck::expr_t const&,
     llvm::Value* dest);
+
+/**
+ * Generates an LLVM value from a runtime function call corresponding to the given application expression.
+ *
+ * @param dest  If this parameter is not nullptr, this function will emit appropriate IR instructions
+ *              to store/memcpy/memset the result of the function call at the runtime location pointed by
+ *              this LLVM value, which must therefore be of an appropriate pointer type.
+ *
+ * @return The generated LLVM value or `dest` if it was not nullptr.
+ */
 llvm::Value* gen_func_call(
     global_context_t&,
     local_context_t const&,

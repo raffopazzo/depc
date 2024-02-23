dbc9782d1b8e2ccb04bf835af246c5bcc2d1916c
Raffaele Rossi <rossi.raffaele@gmail.com>
2024-02-23 18:12:41 +0000

    more doxygen

--- dep0/lib/03_typecheck/src/private/cpp_int_add.hpp
+++ dep0/lib/03_typecheck/src/private/cpp_int_add.hpp
@@ -6,78 +6,156 @@
 
 namespace dep0::typecheck {
 
+/**
+ * Compute the signed-integer-wrapping-addition of the input vaules with bit width of `NumBits`.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside `NumBits`.
+ */
 template <std::size_t NumBits>
 boost::multiprecision::cpp_int
 cpp_int_add_signed(
     boost::multiprecision::cpp_int const&,
     boost::multiprecision::cpp_int const&);
 
+/**
+ * Compute the unsigned-integer-wrapping-addition of the input vaules with bit width of `NumBits`.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside `NumBits`.
+ */
 template <std::size_t NumBits>
 boost::multiprecision::cpp_int
 cpp_int_add_unsigned(
     boost::multiprecision::cpp_int const&,
     boost::multiprecision::cpp_int const&);
 
+/**
+ * Compute the 8-bit signed-integer-wrapping-addition of the input vaules.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside 8-bit.
+ */
 template <>
 boost::multiprecision::cpp_int
 cpp_int_add_signed<8>(
     boost::multiprecision::cpp_int const&,
     boost::multiprecision::cpp_int const&);
 
+/**
+ * Compute the 16-bit signed-integer-wrapping-addition of the input vaules.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside 16-bit.
+ */
 template <>
 boost::multiprecision::cpp_int
 cpp_int_add_signed<16>(
     boost::multiprecision::cpp_int const&,
     boost::multiprecision::cpp_int const&);
 
+/**
+ * Compute the 32-bit signed-integer-wrapping-addition of the input vaules.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside 32-bit.
+ */
 template <>
 boost::multiprecision::cpp_int
 cpp_int_add_signed<32>(
     boost::multiprecision::cpp_int const&,
     boost::multiprecision::cpp_int const&);
 
+/**
+ * Compute the 64-bit signed-integer-wrapping-addition of the input vaules.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside 64-bit.
+ */
 template <>
 boost::multiprecision::cpp_int
 cpp_int_add_signed<64>(
     boost::multiprecision::cpp_int const&,
     boost::multiprecision::cpp_int const&);
 
+/**
+ * Compute the 8-bit unsigned-integer-wrapping-addition of the input vaules.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside 8-bit.
+ */
 template <>
 boost::multiprecision::cpp_int
 cpp_int_add_unsigned<8>(
     boost::multiprecision::cpp_int const&,
     boost::multiprecision::cpp_int const&);
 
+/**
+ * Compute the 16-bit unsigned-integer-wrapping-addition of the input vaules.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside 16-bit.
+ */
 template <>
 boost::multiprecision::cpp_int
 cpp_int_add_unsigned<16>(
     boost::multiprecision::cpp_int const&,
     boost::multiprecision::cpp_int const&);
 
+/**
+ * Compute the 32-bit unsigned-integer-wrapping-addition of the input vaules.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside 32-bit.
+ */
 template <>
 boost::multiprecision::cpp_int
 cpp_int_add_unsigned<32>(
     boost::multiprecision::cpp_int const&,
     boost::multiprecision::cpp_int const&);
 
+/**
+ * Compute the 64-bit unsigned-integer-wrapping-addition of the input vaules.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside 64-bit.
+ */
 template <>
 boost::multiprecision::cpp_int
 cpp_int_add_unsigned<64>(
     boost::multiprecision::cpp_int const&,
     boost::multiprecision::cpp_int const&);
 
+/**
+ * Compute the signed-integer-wrapping-addition of the input vaules with the specified bit width.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside the specified bit width.
+ */
 boost::multiprecision::cpp_int
 cpp_int_add_signed(
     ast::width_t,
     boost::multiprecision::cpp_int const&,
     boost::multiprecision::cpp_int const&);
 
+/**
+ * Compute the unsigned-integer-wrapping-addition of the input vaules with the specified bit width.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside the specified bit width.
+ */
 boost::multiprecision::cpp_int
 cpp_int_add_unsigned(
     ast::width_t,
     boost::multiprecision::cpp_int const&,
     boost::multiprecision::cpp_int const&);
 
+/**
+ * Compute the (signed/unsigned)-integer-wrapping-addition of the input vaules with the specified bit width.
+ *
+ * Because this function is part of the typechecking stage,
+ * both input values will be assumed to have an inital value that fits inside the specified bit width.
+ */
 boost::multiprecision::cpp_int
 cpp_int_add(
     ast::sign_t,
--- dep0/lib/03_typecheck/src/private/cpp_int_limits.hpp
+++ dep0/lib/03_typecheck/src/private/cpp_int_limits.hpp
@@ -6,30 +6,106 @@
 
 namespace dep0::typecheck {
 
+/**
+ * Returns the minimum signed integer value that fits inside the given number of bits.
+ */
 template <std::size_t NumBits> boost::multiprecision::cpp_int cpp_int_min_signed();
+
+/**
+ * Returns the maximum signed integer value that fits inside the given number of bits.
+ */
 template <std::size_t NumBits> boost::multiprecision::cpp_int cpp_int_max_signed();
+
+/**
+ * Returns the maximum unsigned integer value that fits inside the given number of bits.
+ */
 template <std::size_t NumBits> boost::multiprecision::cpp_int cpp_int_max_unsigned();
 
+/**
+ * Returns the minimum value for a signed 8-bit integer.
+ */
 template <> boost::multiprecision::cpp_int cpp_int_min_signed<8>();
+
+/**
+ * Returns the minimum value for a signed 16-bit integer.
+ */
 template <> boost::multiprecision::cpp_int cpp_int_min_signed<16>();
+
+/**
+ * Returns the minimum value for a signed 32-bit integer.
+ */
 template <> boost::multiprecision::cpp_int cpp_int_min_signed<32>();
+
+/**
+ * Returns the minimum value for a signed 64-bit integer.
+ */
 template <> boost::multiprecision::cpp_int cpp_int_min_signed<64>();
 
+/**
+ * Returns the maximum value for a signed 8-bit integer.
+ */
 template <> boost::multiprecision::cpp_int cpp_int_max_signed<8>();
+
+/**
+ * Returns the maximum value for a signed 16-bit integer.
+ */
 template <> boost::multiprecision::cpp_int cpp_int_max_signed<16>();
+
+/**
+ * Returns the maximum value for a signed 32-bit integer.
+ */
 template <> boost::multiprecision::cpp_int cpp_int_max_signed<32>();
+
+/**
+ * Returns the maximum value for a signed 64-bit integer.
+ */
 template <> boost::multiprecision::cpp_int cpp_int_max_signed<64>();
 
+/**
+ * Returns the maximum value for an unsigned 8-bit integer.
+ */
 template <> boost::multiprecision::cpp_int cpp_int_max_unsigned<8>();
+
+/**
+ * Returns the maximum value for an unsigned 16-bit integer.
+ */
 template <> boost::multiprecision::cpp_int cpp_int_max_unsigned<16>();
+
+/**
+ * Returns the maximum value for an unsigned 32-bit integer.
+ */
 template <> boost::multiprecision::cpp_int cpp_int_max_unsigned<32>();
+
+/**
+ * Returns the maximum value for an unsigned 64-bit integer.
+ */
 template <> boost::multiprecision::cpp_int cpp_int_max_unsigned<64>();
 
+/**
+ * Returns the minimum signed integer value that fits inside the given bit width.
+ */
 boost::multiprecision::cpp_int cpp_int_min_signed(ast::width_t);
+
+/**
+ * Returns the maximum signed integer value that fits inside the given bit width.
+ */
 boost::multiprecision::cpp_int cpp_int_max_signed(ast::width_t);
+
+/**
+ * Returns the maximum unsigned integer value that fits inside the given bit width.
+ */
 boost::multiprecision::cpp_int cpp_int_max_unsigned(ast::width_t);
 
+/**
+ * Returns the minimum signed-or-unsigned integer value that fits inside the given bit width.
+ *
+ * For unsigned integers, this will be 0.
+ */
 boost::multiprecision::cpp_int cpp_int_min(ast::sign_t, ast::width_t);
+
+/**
+ * Returns the maximum signed-or-unsigned integer value that fits inside the given bit width.
+ */
 boost::multiprecision::cpp_int cpp_int_max(ast::sign_t, ast::width_t);
 
 } // namespace dep0::typecheck

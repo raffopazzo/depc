85de4e0f001f3ffeb10d2a833aa6fc0dd0e8b45e
Raffaele Rossi <rossi.raffaele@gmail.com>
2024-03-08 19:58:24 +0000

    fix crash calling forward-declared function

--- dep0/lib/05_llvmgen/test/dep0_llvmgen_tests_0009_func_decl.cpp
+++ dep0/lib/05_llvmgen/test/dep0_llvmgen_tests_0009_func_decl.cpp
@@ -34,4 +34,23 @@ BOOST_AUTO_TEST_CASE(pass_001)
     }
 }
 
+BOOST_AUTO_TEST_CASE(pass_002)
+{
+    BOOST_TEST_REQUIRE(pass("0009_func_decl/pass_002.depc"));
+    {
+        auto const f = pass_result.value()->getFunction("f");
+        BOOST_TEST_REQUIRE(is_function_of(f, std::tuple{}, is_i32, sext));
+        BOOST_TEST(f->size() == 0ul);
+    }
+    {
+        auto const f = pass_result.value()->getFunction("g");
+        BOOST_TEST_REQUIRE(is_function_of(f, std::tuple{}, is_i32, sext));
+        BOOST_TEST_REQUIRE(f->size() == 1ul);
+        BOOST_TEST(
+            is_return_of(
+                f->getEntryBlock().getTerminator(),
+                direct_call_of(exactly(pass_result.value()->getFunction("f")))));
+    }
+}
+
 BOOST_AUTO_TEST_SUITE_END()

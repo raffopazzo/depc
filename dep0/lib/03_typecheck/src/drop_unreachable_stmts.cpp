e2e3a053aa8ca4e86859cedcfb342ec5c6b7f98e
Raffaele Rossi <rossi.raffaele@gmail.com>
2024-03-09 18:41:40 +0000

    drop more unreachable statements

--- dep0/lib/03_typecheck/src/drop_unreachable_stmts.cpp
+++ dep0/lib/03_typecheck/src/drop_unreachable_stmts.cpp
@@ -1,5 +1,7 @@
 #include "private/drop_unreachable_stmts.hpp"
 
+#include "private/returns_from_all_branches.hpp"
+
 namespace dep0::typecheck {
 
 bool drop_unreachable_stmts(body_t& body)
@@ -16,7 +18,22 @@ bool drop_unreachable_stmts(body_t& body)
         body.stmts.erase(std::next(it), body.stmts.end());
         changed |= old_size != body.stmts.size();
     }
-    // 2. drop unreachable if-else bodies;
+    // 1bis. similarly to (1) all statements after an if-else that returns from all branches are also unreachable;
+    if (auto const it =
+            std::ranges::find_if(
+                body.stmts,
+                [] (stmt_t const& x)
+                {
+                    auto const if_else = std::get_if<stmt_t::if_else_t>(&x.value);
+                    return if_else and returns_from_all_branches(*if_else);
+                });
+        it != body.stmts.end())
+    {
+        auto const old_size = body.stmts.size();
+        body.stmts.erase(std::next(it), body.stmts.end());
+        changed |= old_size != body.stmts.size();
+    }
+    // 2. drop unreachable if-else bodies, i.e. bodies guarded by a boolean constant of the wrong value
     for (auto& stmt: body.stmts)
         if (auto* const if_else = std::get_if<stmt_t::if_else_t>(&stmt.value))
             if (auto const c = std::get_if<expr_t::boolean_constant_t>(&if_else->cond.value))
--- dep0/lib/03_typecheck/src/private/returns_from_all_branches.hpp
+++ dep0/lib/03_typecheck/src/private/returns_from_all_branches.hpp
@@ -11,4 +11,10 @@ namespace dep0::typecheck {
  */
 bool returns_from_all_branches(body_t const&);
 
+/**
+ * @return  True if the input if-else statement contains both branches and
+ *          both of them contain an explicit return statement.
+ */
+bool returns_from_all_branches(stmt_t::if_else_t const&);
+
 } // namespace dep0::typecheck

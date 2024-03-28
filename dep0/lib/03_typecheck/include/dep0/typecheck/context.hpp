5bfaa4a3369a1ed93646f262b7e8fbd707e46c2f
Raffaele Rossi <rossi.raffaele@gmail.com>
2024-03-28 23:34:22 +0000

    doxygen

--- dep0/lib/03_typecheck/include/dep0/typecheck/context.hpp
+++ dep0/lib/03_typecheck/include/dep0/typecheck/context.hpp
@@ -31,12 +31,38 @@ public:
     context_t& operator=(context_t&&) = default;
 
     // const member functions
+
+    /**
+     * Obtain a fresh context that inherits from the current one, which is referred to as the "parent".
+     * The new context will allow to rebind variable names already bound in the parent context, aka shadowing.
+     */
     context_t extend() const;
+
+    /**
+     * Obtain a new context where all types have been rewritten according to the equality `from = to`.
+     * For example, if `xs` has type `array_t(i32_t, n)`, rewriting from `n` to `2` will
+     * return a new context where `xs` has type `array_t(i32_t, 2)`.
+     */
     context_t rewrite(expr_t const& from, expr_t const& to) const;
+
+    /**
+     * Return the name of all variables visible from the current context,
+     * i.e. all variables in the current context plus all variables from the parent, grand-parent, etc.
+     */
     std::set<expr_t::var_t> vars() const;
+
+    /**
+     * Return the expression bound to the given variable name, or nullptr if no binding exists yet.
+     * The returned pointer is guaranteed stable, i.e. it will never be invalidated.
+     */
     value_type const* operator[](expr_t::var_t const&) const;
 
     // non-const member functions
+
+    /**
+     * Add a new binding variable to the current context level, if one does not already exist.
+     * If a binding already exists, but only at the parent level, the new binding will shadowow the parent one.
+     */
     dep0::expected<const_iterator> try_emplace(expr_t::var_t, std::optional<source_loc_t>, expr_t);
 
 private:

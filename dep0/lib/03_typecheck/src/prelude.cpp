1e49eab675c240d3d2027966542a5a4c4d14b146
Raffaele Rossi <rossi.raffaele@gmail.com>
2024-07-20 00:21:19 +0100

    more prelude

--- dep0/lib/03_typecheck/src/prelude.cpp
+++ dep0/lib/03_typecheck/src/prelude.cpp
@@ -9,27 +9,45 @@ namespace dep0::typecheck {
 
 namespace {
 auto constexpr prelude_text = R"(
-// bool
-axiom eq_true_intro(bool a, true_t(a)) -> true_t(a == true);
-axiom eq_true_elim(bool a, true_t(a == true)) -> true_t(a);
-axiom eq_false_intro(bool a, true_t(not a)) -> true_t(a == false);
-axiom eq_false_elim(bool a, true_t(a == false)) -> true_t(not a);
-axiom not_eq_true_intro(bool a, true_t(not a)) -> true_t(not (a == true));
-axiom not_eq_true_elim(bool a, true_t(not (a == true))) -> true_t(not a);
-axiom not_eq_false_intro(bool a, true_t(a)) -> true_t(not (a == false));
-axiom not_eq_false_elim(bool a, true_t(not (a == false))) -> true_t(a);
-
+// bool (first order logic)
+auto contradiction(bool a, 0 true_t(a), 0 true_t(not a)) -> true_t(false) { if (a) impossible; else impossible; }
+axiom double_neg_intro(bool a, true_t(a)) -> true_t(not not a);
+axiom double_neg_elim(bool a, true_t(not not a)) -> true_t(a);
 axiom conj_intro(bool a, bool b, true_t(a), true_t(b)) -> true_t(a and b);
 axiom conj_elim_a(bool a, bool b, true_t(a and b)) -> true_t(a);
 axiom conj_elim_b(bool a, bool b, true_t(a and b)) -> true_t(b);
 axiom disj_intro_a(bool a, bool b, true_t(a)) -> true_t(a or b);
 axiom disj_intro_b(bool a, bool b, true_t(b)) -> true_t(a or b);
-axiom double_neg_intro(bool a, true_t(a)) -> true_t(not not a);
-axiom double_neg_elim(bool a, true_t(not not a)) -> true_t(a);
 
+// bool (==)
+axiom eq_refl_bool(bool a) -> true_t(a == a);
+axiom eq_symm_bool(bool a, bool b, true_t(a == b)) -> true_t(b == a);
+axiom eq_trans_bool(bool a, bool b, bool c, true_t(a == b), true_t(b == c)) -> true_t(a == c);
+auto eq_true_intro(bool a, 0 true_t(a)) -> true_t(a == true) { if (a) return {}; else impossible; }
+auto eq_true_elim(bool a, 0 true_t(a == true)) -> true_t(a) { if (a) return {}; else impossible; }
+auto eq_false_intro(bool a, 0 true_t(not a)) -> true_t(a == false) { if (a) impossible; else return {}; }
+auto eq_false_elim(bool a, 0 true_t(a == false)) -> true_t(not a) { if (a) impossible; else return {}; }
+axiom not_eq_intro(bool a, bool b, true_t(a != b)) -> true_t(not (a == b));
+axiom not_eq_elim(bool a, bool b, true_t(not (a == b))) -> true_t(a != b);
+auto not_eq_true_intro(bool a, 0 true_t(not a)) -> true_t(not (a == true)) { if (a) impossible; else return {}; }
+auto not_eq_true_elim(bool a, 0 true_t(not (a == true))) -> true_t(not a) { if (a) impossible; else return {}; }
+auto not_eq_false_intro(bool a, 0 true_t(a)) -> true_t(not (a == false)) { if (a) return {}; else impossible; }
+auto not_eq_false_elim(bool a, 0 true_t(not (a == false))) -> true_t(a) { if (a) return {}; else impossible; }
+
+// bool (!=)
+axiom neq_symm_bool(bool a, bool b, true_t(a != b)) -> true_t(b != a);
+auto neq_true_intro(bool a, 0 true_t(not a)) -> true_t(a != true) { if (a) impossible; else return {}; }
+auto neq_true_elim(bool a, 0 true_t(a != true)) -> true_t(not a) { if (a) impossible; else return {}; }
+auto neq_false_intro(bool a, 0 true_t(a)) -> true_t(a != false) { if (a) return {}; else impossible; }
+auto neq_false_elim(bool a, 0 true_t(a != false)) -> true_t(a) { if (a) return {}; else impossible; }
+axiom not_neq_intro(bool a, bool b, true_t(a == b)) -> true_t(not (a != b));
+axiom not_neq_elim(bool a, bool b, true_t(not (a != b))) -> true_t(a == b);
+auto not_neq_true_intro(bool a, 0 true_t(a)) -> true_t(not (a != true)) { if (a) return {}; else impossible; }
+
+// bool (<)
 axiom nothing_smaller_than_false(bool a, true_t(a < false)) -> true_t(false);
-axiom nothing_bigger_than_true(bool a, true_t(a > true)) -> true_t(false);
-
+axiom not_lt_intro(bool a, bool b, true_t(a >= b)) -> true_t(not (a < b));
+axiom not_lt_elim(bool a, bool b, true_t(not (a < b))) -> true_t(a >= b);
 auto a_lt_b_implies_b(bool a, bool b, 0 true_t(a < b)) -> true_t(b)
 {
     if (b)
@@ -40,6 +58,16 @@ auto a_lt_b_implies_b(bool a, bool b, 0 true_t(a < b)) -> true_t(b)
         impossible;
 }
 
+// bool (<=)
+axiom lte_refl_bool(bool a) -> true_t(a <= a);
+axiom lte_tran_bool(bool a, bool b, bool c, true_t(a <= b), true_t(b <= c)) -> true_t(a <= c);
+axiom true_biggest_bool(bool a) -> true_t(a <= true);
+axiom false_smallest_bool(bool a) -> true_t(false <= a);
+
+// bool (>)
+axiom nothing_bigger_than_true(bool a, true_t(a > true)) -> true_t(false);
+axiom not_gt_intro(bool a, bool b, true_t(a <= b)) -> true_t(not (a > b));
+axiom not_gt_elim(bool a, bool b, true_t(not (a > b))) -> true_t(a <= b);
 auto a_gt_b_implies_a(bool a, bool b, 0 true_t(a > b)) -> true_t(a)
 {
     if (b)
@@ -50,7 +78,14 @@ auto a_gt_b_implies_a(bool a, bool b, 0 true_t(a > b)) -> true_t(a)
         impossible;
 }
 
-// axiom gt_implies_lt_64(u64_t a, u64_t b, true_t(a > b)) -> true_t(b < a);
+// bool (>=)
+axiom gte_refl_bool(bool a) -> true_t(a >= a);
+axiom gte_tran_bool(bool a, bool b, bool c, true_t(a >= b), true_t(b >= c)) -> true_t(a >= c);
+
+axiom lt_gt_commute_bool(bool a, bool b, true_t(a < b)) -> true_t(b > a);
+axiom lte_gte_commute_bool(bool a, bool b, true_t(a <= b)) -> true_t(b >= a);
+axiom gt_lt_commute_bool(bool a, bool b, true_t(a > b)) -> true_t(b < a);
+axiom gte_lte_commute_bool(bool a, bool b, true_t(a >= b)) -> true_t(b <= a);
 )";
 } // namespace
 

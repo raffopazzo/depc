385e4707d467b24436e828b56d4cfa7f8c576401
Raffaele Rossi <rossi.raffaele@gmail.com>
2024-08-21 18:12:06 +0100

    Update dep0/lib/03_typecheck/src/type_assign.cpp

--- dep0/lib/03_typecheck/src/type_assign.cpp
+++ dep0/lib/03_typecheck/src/type_assign.cpp
@@ -284,8 +284,7 @@ type_assign(
         {
             if (auto var = expr_t::var_t{x.name}; auto const expr = ctx[var])
             {
-                // if multiplier is zero nothing is really used and type-assignment always succeeds;
-                // so usage is only checked when multiplier is non-zero
+                // if multiplier is zero type-assignment always succeeds, so check only if multiplier is non-zero
                 if (usage_multiplier > ast::qty_t::zero)
                 {
                     auto const prev = usage[var];

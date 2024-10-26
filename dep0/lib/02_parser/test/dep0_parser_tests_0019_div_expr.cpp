cbcbd70a96c231bcb8d75efaed5b17e586b3e44f
Raffaele Rossi <rossi.raffaele@gmail.com>
2024-10-26 15:11:19 +0100

    add tests

---
+++ dep0/lib/02_parser/test/dep0_parser_tests_0019_div_expr.cpp
@@ -0,0 +1,174 @@
+#define BOOST_TEST_MODULE dep0_parser_tests_0019_div_expr
+#include <boost/test/unit_test.hpp>
+
+#include "parser_tests_fixture.hpp"
+
+using namespace dep0::testing;
+
+BOOST_FIXTURE_TEST_SUITE(dep0_parser_tests_0019_div_expr, ParserTestsFixture)
+
+BOOST_AUTO_TEST_CASE(pass_000)
+{
+    BOOST_TEST_REQUIRE(pass("0019_div_expr/pass_000.depc"));
+    BOOST_TEST_REQUIRE(pass_result->entries.size() == 1ul);
+    auto const f = std::get_if<dep0::parser::func_def_t>(&pass_result->entries[0]);
+    BOOST_TEST_REQUIRE(f);
+    BOOST_TEST(f->name == "two");
+    BOOST_TEST(f->value.args.size() == 0ul);
+    BOOST_TEST(is_i32(f->value.ret_type.get()));
+    BOOST_TEST_REQUIRE(f->value.body.stmts.size() == 1ul);
+    BOOST_TEST(is_return_of(f->value.body.stmts[0ul], div(constant(2), constant(1))));
+}
+
+BOOST_AUTO_TEST_CASE(pass_001)
+{
+    BOOST_TEST_REQUIRE(pass("0019_div_expr/pass_001.depc"));
+    BOOST_TEST_REQUIRE(pass_result->entries.size() == 1ul);
+    auto const f = std::get_if<dep0::parser::func_def_t>(&pass_result->entries[0]);
+    BOOST_TEST_REQUIRE(f);
+    BOOST_TEST(f->name == "minus_two");
+    BOOST_TEST(f->value.args.size() == 0ul);
+    BOOST_TEST(is_i32(f->value.ret_type.get()));
+    BOOST_TEST_REQUIRE(f->value.body.stmts.size() == 1ul);
+    BOOST_TEST(is_return_of(f->value.body.stmts[0ul], div(constant(2), constant(-1))));
+}
+
+BOOST_AUTO_TEST_CASE(pass_002)
+{
+    BOOST_TEST_REQUIRE(pass("0019_div_expr/pass_002.depc"));
+    BOOST_TEST_REQUIRE(pass_result->entries.size() == 1ul);
+    auto const f = std::get_if<dep0::parser::func_def_t>(&pass_result->entries[0]);
+    BOOST_TEST_REQUIRE(f);
+    BOOST_TEST(f->name == "two");
+    BOOST_TEST(f->value.args.size() == 0ul);
+    BOOST_TEST(is_i32(f->value.ret_type.get()));
+    BOOST_TEST_REQUIRE(f->value.body.stmts.size() == 1ul);
+    BOOST_TEST(is_return_of(f->value.body.stmts[0ul], div(constant(2), constant(1))));
+}
+
+BOOST_AUTO_TEST_CASE(pass_003)
+{
+    BOOST_TEST_REQUIRE(pass("0019_div_expr/pass_003.depc"));
+    BOOST_TEST_REQUIRE(pass_result->entries.size() == 7ul);
+    BOOST_TEST(std::holds_alternative<dep0::parser::axiom_t>(pass_result->entries[0]));
+    {
+        auto const f = std::get_if<dep0::parser::func_def_t>(&pass_result->entries[1]);
+        BOOST_TEST_REQUIRE(f);
+        BOOST_TEST(f->name == "f1");
+        BOOST_TEST_REQUIRE(f->value.args.size() == 3ul);
+        BOOST_TEST(is_arg(f->value.args[0], is_i32, "a"));
+        BOOST_TEST(is_arg(f->value.args[1], is_i32, "b"));
+        BOOST_TEST(is_arg(f->value.args[2], is_i32, "c"));
+        BOOST_TEST(is_i32(f->value.ret_type.get()));
+        BOOST_TEST_REQUIRE(f->value.body.stmts.size() == 1ul);
+        BOOST_TEST(is_return_of(f->value.body.stmts[0ul], plus(div(var("a"), var("b")), var("c"))));
+    }
+    {
+        auto const f = std::get_if<dep0::parser::func_def_t>(&pass_result->entries[2]);
+        BOOST_TEST_REQUIRE(f);
+        BOOST_TEST(f->name == "f2");
+        BOOST_TEST_REQUIRE(f->value.args.size() == 3ul);
+        BOOST_TEST(is_arg(f->value.args[0], is_i32, "a"));
+        BOOST_TEST(is_arg(f->value.args[1], is_i32, "b"));
+        BOOST_TEST(is_arg(f->value.args[2], is_i32, "c"));
+        BOOST_TEST(is_i32(f->value.ret_type.get()));
+        BOOST_TEST_REQUIRE(f->value.body.stmts.size() == 1ul);
+        BOOST_TEST(is_return_of(f->value.body.stmts[0ul], plus(var("a"), div(var("b"), var("c")))));
+    }
+    {
+        auto const f = std::get_if<dep0::parser::func_def_t>(&pass_result->entries[3]);
+        BOOST_TEST_REQUIRE(f);
+        BOOST_TEST(f->name == "f3");
+        BOOST_TEST_REQUIRE(f->value.args.size() == 3ul);
+        BOOST_TEST(is_arg(f->value.args[0], is_i32, "a"));
+        BOOST_TEST(is_arg(f->value.args[1], is_i32, "b"));
+        BOOST_TEST(is_arg(f->value.args[2], is_i32, "c"));
+        BOOST_TEST(is_i32(f->value.ret_type.get()));
+        BOOST_TEST_REQUIRE(f->value.body.stmts.size() == 1ul);
+        BOOST_TEST(is_return_of(f->value.body.stmts[0ul], minus(var("a"), div(var("b"), var("c")))));
+    }
+    {
+        auto const f = std::get_if<dep0::parser::func_def_t>(&pass_result->entries[4]);
+        BOOST_TEST_REQUIRE(f);
+        BOOST_TEST(f->name == "f4");
+        BOOST_TEST_REQUIRE(f->value.args.size() == 3ul);
+        BOOST_TEST(is_arg(f->value.args[0], is_i32, "a"));
+        BOOST_TEST(is_arg(f->value.args[1], is_i32, "b"));
+        BOOST_TEST(is_arg(f->value.args[2], is_i32, "c"));
+        BOOST_TEST(is_i32(f->value.ret_type.get()));
+        BOOST_TEST_REQUIRE(f->value.body.stmts.size() == 1ul);
+        BOOST_TEST(is_return_of(f->value.body.stmts[0ul], minus(div(var("a"), var("b")), var("c"))));
+    }
+    {
+        auto const f = std::get_if<dep0::parser::func_def_t>(&pass_result->entries[5]);
+        BOOST_TEST_REQUIRE(f);
+        BOOST_TEST(f->name == "f5");
+        BOOST_TEST_REQUIRE(f->value.args.size() == 3ul);
+        BOOST_TEST(is_arg(f->value.args[0], is_i32, "a"));
+        BOOST_TEST(is_arg(f->value.args[1], is_i32, "b"));
+        BOOST_TEST(is_arg(f->value.args[2], is_i32, "c"));
+        BOOST_TEST(is_i32(f->value.ret_type.get()));
+        BOOST_TEST_REQUIRE(f->value.body.stmts.size() == 1ul);
+        BOOST_TEST(is_return_of(f->value.body.stmts[0ul], mult(div(var("a"), var("b")), var("c"))));
+    }
+    {
+        auto const f = std::get_if<dep0::parser::func_def_t>(&pass_result->entries[6]);
+        BOOST_TEST_REQUIRE(f);
+        BOOST_TEST(f->name == "f6");
+        BOOST_TEST_REQUIRE(f->value.args.size() == 3ul);
+        BOOST_TEST(is_arg(f->value.args[0], is_i32, "a"));
+        BOOST_TEST(is_arg(f->value.args[1], is_i32, "b"));
+        BOOST_TEST(is_arg(f->value.args[2], is_i32, "c"));
+        BOOST_TEST(is_i32(f->value.ret_type.get()));
+        BOOST_TEST_REQUIRE(f->value.body.stmts.size() == 1ul);
+        BOOST_TEST(is_return_of(f->value.body.stmts[0ul], div(mult(var("a"), var("b")), var("c"))));
+    }
+}
+
+BOOST_AUTO_TEST_CASE(typecheck_error_000)
+{
+    BOOST_TEST_REQUIRE(pass("0019_div_expr/typecheck_error_000.depc"));
+    BOOST_TEST_REQUIRE(pass_result->entries.size() == 1ul);
+    auto const f = std::get_if<dep0::parser::func_def_t>(&pass_result->entries[0]);
+    BOOST_TEST_REQUIRE(f);
+    BOOST_TEST(f->name == "minus_two");
+    BOOST_TEST(f->value.args.size() == 0ul);
+    BOOST_TEST_REQUIRE(f->value.body.stmts.size() == 1ul);
+    BOOST_TEST(is_return_of(f->value.body.stmts[0ul], div(constant(2), constant(-1))));
+}
+
+BOOST_AUTO_TEST_CASE(typecheck_error_001)
+{
+    BOOST_TEST_REQUIRE(pass("0019_div_expr/typecheck_error_001.depc"));
+    BOOST_TEST_REQUIRE(pass_result->entries.size() == 1ul);
+    {
+        auto const f = std::get_if<dep0::parser::func_def_t>(&pass_result->entries[0]);
+        BOOST_TEST_REQUIRE(f);
+        BOOST_TEST(f->name == "f");
+        BOOST_TEST_REQUIRE(f->value.args.size() == 2ul);
+        BOOST_TEST(is_arg(f->value.args[0], is_i32, "a"));
+        BOOST_TEST(is_arg(f->value.args[1], is_i32, "b"));
+        BOOST_TEST(is_i32(f->value.ret_type.get()));
+        BOOST_TEST_REQUIRE(f->value.body.stmts.size() == 1ul);
+        BOOST_TEST(is_return_of(f->value.body.stmts[0ul], div(var("a"), var("b"))));
+    }
+}
+
+BOOST_AUTO_TEST_CASE(typecheck_error_002)
+{
+    BOOST_TEST_REQUIRE(pass("0019_div_expr/typecheck_error_002.depc"));
+    BOOST_TEST_REQUIRE(pass_result->entries.size() == 1ul);
+    {
+        auto const f = std::get_if<dep0::parser::func_def_t>(&pass_result->entries[0]);
+        BOOST_TEST_REQUIRE(f);
+        BOOST_TEST(f->name == "f");
+        BOOST_TEST_REQUIRE(f->value.args.size() == 2ul);
+        BOOST_TEST(is_arg(f->value.args[0], is_u32, "a"));
+        BOOST_TEST(is_arg(f->value.args[1], is_u32, "b"));
+        BOOST_TEST(is_u32(f->value.ret_type.get()));
+        BOOST_TEST_REQUIRE(f->value.body.stmts.size() == 1ul);
+        BOOST_TEST(is_return_of(f->value.body.stmts[0ul], div(var("a"), var("b"))));
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ dep0/lib/05_llvmgen/test/llvm_predicates/div.cpp
@@ -0,0 +1,1 @@
+#include "llvm_predicates/div.hpp"

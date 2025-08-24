44a8caa9c043362d32710d42a2fae19c9e9c4027
Raffaele Rossi <rossi.raffaele@gmail.com>
2025-08-24 23:02:59 +0100

    build jump table in match()

--- dep0/lib/00_core/include/dep0/match.hpp
+++ dep0/lib/00_core/include/dep0/match.hpp
@@ -10,81 +10,37 @@
  */
 #pragma once
 
-#include <cassert>
-#include <concepts>
+#include <array>
+#include <type_traits>
 #include <variant>
+
+#include <boost/hana.hpp>
 
 namespace dep0 {
 
-/** @cond DEP0_DOXYGEN_HIDE */
-namespace detail::match {
-
-template <typename T, typename F, typename... Fs>
-requires (std::invocable<F, T> or sizeof...(Fs) > 0ul)
-decltype(auto) invoke_one(T&& x, F&& f, Fs&&... fs)
-{
-    if constexpr (std::invocable<F, T>)
-        return std::forward<F>(f)(std::forward<T>(x));
-    else
-        return invoke_one(std::forward<T>(x), std::forward<Fs>(fs)...);
-}
-
-template <std::size_t I, typename... Ts, typename... Fs>
-decltype(auto) impl(std::integral_constant<std::size_t, I>, std::variant<Ts...> const& x, Fs&&... fs)
-{
-    auto const* const p = std::get_if<I>(&x);
-    if constexpr (I+1 < sizeof...(Ts))
-    {
-        if (p)
-            return invoke_one(*p, std::forward<Fs>(fs)...);
-        else
-            return impl(std::integral_constant<std::size_t, I+1>{}, x, std::forward<Fs>(fs)...);
-    }
-    else
-    {
-        assert(p and "corrupted variant uninhabited by any of its types");
-        return invoke_one(*p, std::forward<Fs>(fs)...);
-    }
-}
-
-template <std::size_t I, typename... Ts, typename... Fs>
-decltype(auto) impl(std::integral_constant<std::size_t, I>, std::variant<Ts...>& x, Fs&&... fs)
-{
-    auto* const p = std::get_if<I>(&x);
-    if constexpr (I+1 < sizeof...(Ts))
-    {
-        if (p)
-            return invoke_one(*p, std::forward<Fs>(fs)...);
-        else
-            return impl(std::integral_constant<std::size_t, I+1>{}, x, std::forward<Fs>(fs)...);
-    }
-    else
-    {
-        assert(p and "corrupted variant uninhabited by any of its types");
-        return invoke_one(*p, std::forward<Fs>(fs)...);
-    }
-}
-
-} // namespace detail::match
-/** @endcond */
-
 /**
- * @brief Invokes the first function that can accept the value currently stored in the input variant.
+ * @brief Invokes the best candidate that can accept the value currently stored in the input variant.
  *
- * It is a compile-time error if any of the variant alternatives cannot be accepted by at least one function.
- *
- * If more than one function can accept the same variant alternative, only the first function will be invoked.
- *
- * @remarks This is **not** equivalent to calling `std::visit()` with an overloaded visitor function because
- * the function invoked is the first viable one, not the best candidate according to overload resolution rules.
- * Instead, this is equivalent to calling `std::visit()` with the result of `boost::hana::overload_linearly()`.
+ * The invoked function is picked according to standard overload resolution rules.
  *
  * @return The result returned by the invoked function.
  */
 template <typename... Ts, typename... Fs>
 decltype(auto) match(std::variant<Ts...> const& x, Fs&&... fs)
 {
-    return detail::match::impl(std::integral_constant<std::size_t, 0>{}, x, std::forward<Fs>(fs)...);
+    using F = decltype(boost::hana::overload(std::forward<Fs>(fs)...));
+    using R = decltype(std::declval<F>()(std::get<0>(x)));
+    auto const jump_table = [&] <std::size_t... Is> (std::index_sequence<Is...>)
+    {
+        static_assert((std::is_same_v<R, decltype(std::declval<F>()(std::get<Is>(x)))> or ...));
+        return std::array{
+            (+[] (std::variant<Ts...> const& x, F&& f) -> R
+            {
+                return std::forward<F>(f)(std::get<Is>(x));
+            })...
+        };
+    }(std::make_index_sequence<sizeof...(Ts)>{});
+    return jump_table[x.index()](x, boost::hana::overload(std::forward<Fs>(fs)...));
 }
 
 /**
@@ -95,7 +51,19 @@ decltype(auto) match(std::variant<Ts...> const& x, Fs&&... fs)
 template <typename... Ts, typename... Fs>
 decltype(auto) match(std::variant<Ts...>& x, Fs&&... fs)
 {
-    return detail::match::impl(std::integral_constant<std::size_t, 0>{}, x, std::forward<Fs>(fs)...);
+    using F = decltype(boost::hana::overload(std::forward<Fs>(fs)...));
+    using R = decltype(std::declval<F>()(std::get<0>(x)));
+    auto const jump_table = [&] <std::size_t... Is> (std::index_sequence<Is...>)
+    {
+        static_assert((std::is_same_v<R, decltype(std::declval<F>()(std::get<Is>(x)))> or ...));
+        return std::array{
+            (+[] (std::variant<Ts...>& x, F&& f) -> R
+            {
+                return std::forward<F>(f)(std::get<Is>(x));
+            })...
+        };
+    }(std::make_index_sequence<sizeof...(Ts)>{});
+    return jump_table[x.index()](x, boost::hana::overload(std::forward<Fs>(fs)...));
 }
 
 } // namespace dep0

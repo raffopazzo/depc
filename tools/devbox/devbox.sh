d344127fc743073280bf448883a3566e727ab856
Raffaele Rossi <rossi.raffaele@gmail.com>
2025-08-29 21:25:44 +0100

    allow linking from within devbox

--- tools/devbox/devbox.sh
+++ tools/devbox/devbox.sh
@@ -8,6 +8,7 @@ script_dir=$(dirname $0)
 repo_dir=$(realpath ${script_dir}/../..)
 docker run \
     --rm -it \
+    --privileged \
     --user $(id -u):$(id -g) \
     -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro \
     -v ${repo_dir}:${repo_dir} \
